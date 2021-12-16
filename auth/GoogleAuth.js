00693ca530a679b306e5c0a90eafd78b7b4cd833
karthikq <kk968346@gmail.com>
2021-12-16 17:45:50 +0530

    resp22

--- auth/GoogleAuth.js
+++ auth/GoogleAuth.js
@@ -18,8 +18,9 @@ module.exports = async function GoogleAuth(passport) {
     passport.use(
       new GoogleStrategy(
         {
-          clientID: " ",
-          clientSecret: " ",
+          clientID:
+            "201697071575-optgp6nu08ph45benq3gc8u687q018sg.apps.googleusercontent.com",
+          clientSecret: "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",
           callbackURL: `${apiUrl()}/auth/user/redirect`,
         },
         async function (accessToken, refreshToken, profile, cb) {
